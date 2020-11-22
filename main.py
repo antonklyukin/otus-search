0201f890657abf9162a11dbdb66c5b68750aaf37
Anton Klyukin <antonklyukin@gmail.com>
2020-11-23 00:21:13 +0300

    First variant of fully working links parser

--- main.py
+++ main.py
@@ -2,34 +2,34 @@
 # -*- coding: utf-8 -*-
 
 
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlunsplit
 import argparse
 import bs4
 import requests
 import re
 
+
 # python3 main.py автомобиль yandex.ru 30 -r csv
 
 
 def get_args():
-
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "keyword",
+        'keyword',
         type=str,
         default="Юпитер",
         help="enter search keyword (Default: Юпитер)",
     )
     parser.add_argument(
-        "searchengine",
+        'search_engine',
         type=str,
         default="google.com",
         choices=["google.com", "yandex.ru"],
         help="select search engine (Default: wikipedia.org)",
     )
     parser.add_argument(
-        "number", type=int, default=30, help="enter number of results (Default: 30)"
+        'number', type=int, default=30, help="enter number of results (Default: 30)"
     )
     parser.add_argument(
         "-r",
@@ -47,7 +47,8 @@ def get_args():
 
     return parser.parse_args()
 
-class Link:
+
+class HtmlLink:
     """
     Класс гиперссылки
     """
@@ -71,10 +72,10 @@ class Link:
         self.__text = text
 
     def __repr__(self):
-        print('URL: ' + str(self.get_url()) + '\n' + 'URL Text: ' + str(self.get_text()))
+        return 'URL: ' + str(self.get_url()) + '\n' + 'URL Text: ' + str(self.get_text())
 
 
-class Page:
+class HtmlPage:
     """
     Класс страницы
     """
@@ -93,30 +94,58 @@ class Page:
     def get_url(self):
         return self.__url
 
-    def __init__(self, url):
+    def __init__(self, url, html_code):
         self.__url = url
-        self.get_page_html_code()
-
-    def get_page_html_code(self):
-        headers_get = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Accept-Language": "ru_RU,en-US,en,ru;q=0.5",
-            "Accept-Encoding": "gzip, deflate",
-            "DNT": "1",
-            "Connection": "keep-alive",
-            "Upgrade-Insecure-Requests": "1",
-        }
-
-        session = requests.Session()
-        req = session.get(self.__url, headers=headers_get)
-        req.encoding = req.apparent_encoding
-
-        self.__html_code = req.text
+        self.__html_code = html_code
 
 
+def get_html_page_code(url: HtmlLink) -> str:
+    """
+    Функция возвращает HTML-код страницы, либо None, если произошла ошибка.
+    :param url: экземпляр класса HtmlLink
+    :return: HTML-код страницы
+    """
+    href = url.get_url()
+    headers_get = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "ru_RU,en-US,en,ru;q=0.5",
+        "Accept-Encoding": "gzip, deflate",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+    }
 
-def get_page_from_file(page="google_req.html"):
+    session = requests.Session()
+    try:
+        req = session.get(href, headers=headers_get, timeout=5)
+    except requests.exceptions.RequestException.Timeout as e:
+        print('Connection timeout: ' + e)
+        return
+    except requests.exceptions.ConnectionError as e:
+        print('Connection error: ' + e)
+        return
+
+    req.encoding = req.apparent_encoding
+
+    return req.text
+
+
+def html_page_builder(url: HtmlLink) -> HtmlPage:
+    """
+    Функция построения экземпляра класса HtmlPage
+    :param url: Экземпляр класса HtmlLink
+    :return: Экземпляр класса HtmlPage
+    """
+
+    page_html_code = get_html_page_code(url)
+    if page_html_code:
+        return HtmlPage(url.get_url(), page_html_code)
+    else:
+        return
+
+
+def get_page_from_file(page='google_req.html'):
     """
     Функция извлечения страницы по ссылке
     """
@@ -157,67 +186,99 @@ def parse_yandex_links():
     for list_item in li_list:
         a_tag = list_item.find("a")
         a_tag_href = a_tag.attrs["href"]
-        a_is_from_yandex = True if "yandex.ru" in a_tag_href else False
+        a_is_from_yandex = True if 'yandex.ru' in a_tag_href else False
         if not a_is_from_yandex:
-            link = Link(str.lower(a_tag_href), a_tag.text)
+            link = HtmlLink(str.lower(a_tag_href), a_tag.text)
             links_list.append(link)
 
     return links_list
 
 
 def urls_reader(page_html_code):
-    page = bs4.BeautifulSoup(page_html_code, "lxml")
+    page = bs4.BeautifulSoup(page_html_code, 'lxml')
 
-    a_tags = page.find_all("a")
+    a_tags = page.find_all('a')
 
-    # if len(a_tags) < 1:
-    #     raise ValueError('No anchor tags found')
+    if len(a_tags) < 1:
+        raise ValueError('No anchor tags found')
 
     for a_tag in a_tags:
         yield a_tag
 
 
-def get_domain_from_url(url):
-    return urlparse(url).netloc
+def clear_href_text(href_text):
+    clean_href_text = " ".join(href_text.split())
+    clean_href_text = clean_href_text.strip()
+    return clean_href_text
 
 
-def get_urls_from_page(page):
+def get_urls_from_page(page: HtmlPage):
+    found_urls_list = list()
 
-    domain = get_domain_from_url(page.get_url())
+    domain = urlparse(page.get_url()).netloc
 
-    reg = re.compile(r"[^\s\n]+")
+    for url in urls_reader(page.get_html_code()):
+        # Отлавливаем ссылки без адресов, ссылки с адресами-заглушками или ссылки из непечатных символов
+        if not ('href' in url.attrs) or (url.attrs['href'] == '#'):
+            continue
 
-    for url in urls_reader(page.get_page_html_code()):
-        if not ('href' in url.attrs):
-            next
-        if url.attrs['href'].startswith("/"):
-            url.attrs['href'] = domain + url.attrs['href']
-        if (reg.match(url.attrs['href'])) and ("#" not in url.attrs['href']):
-            print(url.text, url.attrs['href'], "\n")
+        url_address = urlparse(url.attrs['href'])
+
+        # Отлавливаем всякие tel: в схеме
+        if url_address.scheme and ('http' not in url_address.scheme):
+            continue
+
+        scheme = url_address.scheme if url_address.scheme else 'http'
+        netloc = url_address.netloc if url_address.netloc else domain
+
+        new_url_address = str()
+        # Генерируем новый URL, если в исходном нет схемы и домена
+        if (not url_address.netloc) or (not url_address.scheme):
+            new_url_address = urlunsplit([scheme, netloc, str.strip(url_address.path),
+                                          str.strip(url_address.query), str.strip(url_address.fragment)])
+
+        # Проверка ссылки на ссылку на себя, пропускаем такие ссылки
+        url_regex = re.compile(r'^https?://' + domain + r'/?$')
+        if url_regex.match(new_url_address):
+            continue
+        if new_url_address == '':
+            continue
+
+        url = HtmlLink(new_url_address, url.text)
+        found_urls_list.append(url)
+
+    return found_urls_list
 
 
 def main():
     args = get_args()
 
     keyword = args.keyword
-    searchengine = args.searchengine
+    search_engine = args.search_engine
     number = args.number
     recursive = args.recursive
     export_format = args.export_format
 
     urls_list = parse_yandex_links()
 
+    html_pages_list = list()
+
+    for url in urls_list:
+        page = html_page_builder(url)
+        if page:
+            html_pages_list.append(page)
+
+    urls_list = list()
+
+    for page in html_pages_list:
+        page_links = get_urls_from_page(page)
+        urls_list.extend(page_links)
+
     print(urls_list)
-
-    # test_page = Page(urls_list[1][0])
-
-    # get_urls_from_page(test_page)
-
-
 
 
 if __name__ == "__main__":
     main()
 
 # Александр Телеграм
-# +16692138826 
+# +16692138826
